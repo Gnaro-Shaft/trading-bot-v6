9118a1a9276b78518f4538afa4ed228ee0e2f58c
Gnaro-Shaft <gced.nisusgmail.com>
2025-05-02 10:19:47 +0200

    ajout status telegram

--- status.py
+++ status.py
@@ -1,13 +1,14 @@
 # status.py
 from memory import get_open_trades
 from binance_api import get_current_price
+from notifier import send_telegram_message
 
 trades = get_open_trades()
 current_price = get_current_price()
 
-print("\n📊 Positions ouvertes :\n")
+message = "\n📊 Positions ouvertes :\n"
 if not trades:
-    print("Aucune position enregistrée.")
+    message += "Aucune position enregistrée."
 else:
     for i, trade in enumerate(trades):
         buy_price = float(trade['price'])
@@ -15,6 +16,9 @@ else:
         date = trade['timestamp']
         gain = (current_price - buy_price) / buy_price * 100
 
-        print(f"[{i}] {quantity} BTC acheté à {buy_price} USDC le {date[:19]} → Gain latent : {gain:.2f}%")
+        message += f"[{i}] {quantity} BTC acheté à {buy_price} USDC le {date[:19]} → Gain latent : {gain:.2f}%\n"
 
-print(f"\n📈 Prix actuel : {current_price} USDC")
+message += f"\n📈 Prix actuel : {current_price} USDC"
+
+print(message)
+send_telegram_message(message)
